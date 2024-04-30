ebd63b2e8842f19ad80aaf24dd1287e9a4cd0efa
SaoudAlawi <46417422+SaoudAlawi@users.noreply.github.com>
2024-04-30 12:03:26 +0400

    mod job title

--- app.py
+++ app.py
@@ -114,8 +114,8 @@ with tabs[3]:
     )
     container = st.container()
     team_members = [
-    {"name": "Salim", "job_title": "Software Engineer", "image_url": "salim.jpeg"},
-    {"name": "Ahmed", "job_title": "AI Engineer / Designer", "image_url": "ahmed.jpeg"},
+    {"name": "Salim", "job_title": "AI Engineer", "image_url": "salim.jpeg"},
+    {"name": "Ahmed", "job_title": "Data Scientist", "image_url": "ahmed.jpeg"},
     {"name": "Madona", "job_title": "CEO", "image_url": "madona.jpg"},
     {"name": "Saud", "job_title": "Blockchain Engineer/the GOAT", "image_url": "saud.jpeg"},
     {"name": "Ghadeer", "job_title": "Software Engineer ", "image_url": "ghadeer.jpg"},
